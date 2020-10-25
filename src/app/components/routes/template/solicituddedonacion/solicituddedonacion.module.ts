7d033e1f0b455bbf70e45c4dbf0fbcdc99d7a691
Martin Lora <martinlorajorge@gmail.com>
2020-10-25 04:41:04 -0300

    terminado trabajo UI detalle de componente(Mariano te toca a vos jaja)

--- src/app/components/routes/template/solicituddedonacion/solicituddedonacion.module.ts
+++ src/app/components/routes/template/solicituddedonacion/solicituddedonacion.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SolicituddedonacionComponent } from './solicituddedonacion/solicituddedonacion.component';
 import { Routes, RouterModule } from '@angular/router';
+import { DetallededonacionModule } from '../components/detallededonacion/detallededonacion.module';
 
 
 
 const routes: Routes = [
   { path: 'solicitud', component: SolicituddedonacionComponent },
+  { path: 'detalle', component: DetallededonacionModule },
 ];
 
 @NgModule({
@@ -14,6 +16,7 @@ const routes: Routes = [
     SolicituddedonacionComponent
   ],
   imports: [
+    DetallededonacionModule,
     RouterModule.forChild(routes),
     CommonModule
   ],
