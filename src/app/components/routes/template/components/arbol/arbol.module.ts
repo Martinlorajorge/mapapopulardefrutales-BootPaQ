2cf03b70d4f34c9f1eac1353b34e30984f463a3c
Martin Lora <martinlorajorge@gmail.com>
2020-10-25 01:28:17 -0300

    pasar por la card al hacer click el boton una solicitud (pantalla)

--- src/app/components/routes/template/components/arbol/arbol.module.ts
+++ src/app/components/routes/template/components/arbol/arbol.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArbolComponent } from './arbol/arbol.component';
+import { SolicituddedonacionModule } from '../../solicituddedonacion/solicituddedonacion.module';
+import { Routes, RouterModule } from '@angular/router';
 
 
+const routes: Routes = [
+  { path: 'solicitud', component: SolicituddedonacionModule },
+];
 
 @NgModule({
-  declarations: [ArbolComponent],
+  declarations: [
+    ArbolComponent
+  ],
   imports: [
+    RouterModule.forChild(routes),
+    SolicituddedonacionModule,
     CommonModule
   ],
   exports: [
--- src/app/components/routes/template/solicituddedonacion/solicituddedonacion.module.ts
+++ src/app/components/routes/template/solicituddedonacion/solicituddedonacion.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SolicituddedonacionComponent } from './solicituddedonacion/solicituddedonacion.component';
+import { Routes, RouterModule } from '@angular/router';
 
 
+const routes: Routes = [
+  { path: 'solicitud', component: SolicituddedonacionComponent },
+];
 
 @NgModule({
   declarations: [
     SolicituddedonacionComponent
   ],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
   ],
   exports: [
