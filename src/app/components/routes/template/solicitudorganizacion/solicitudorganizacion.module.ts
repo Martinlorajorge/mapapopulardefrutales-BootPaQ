a2080316b410ba1bb66cb134627f35eed96002b7
Martin Lora <martinlorajorge@gmail.com>
2020-10-27 18:12:06 -0300

    terminado solicitud organizacion (ya creo que tenes todas las Pantallas )

--- src/app/components/routes/template/solicitudorganizacion/solicitudorganizacion.module.ts
+++ src/app/components/routes/template/solicitudorganizacion/solicitudorganizacion.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SolicitudorganizacionComponent } from './solicitudorganizacion/solicitudorganizacion.component';
 import { Routes, RouterModule } from '@angular/router';
+import { CardrecolectorModule } from '../components/cardrecolector/cardrecolector.module';
+import { SolicituddedonacionModule } from '../solicituddedonacion/solicituddedonacion.module';
+
+
+
 
 const routes: Routes = [
 
   { path: 'peticion', component: SolicitudorganizacionComponent },
+  { path: 'Cardrecolector', component: CardrecolectorModule },
+  { path: 'solicitud', component: SolicituddedonacionModule },
+
+
 
 ];
 
@@ -16,8 +25,12 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [SolicitudorganizacionComponent],
+  declarations: [
+    SolicitudorganizacionComponent
+  ],
   imports: [
+    SolicituddedonacionModule,
+    CardrecolectorModule,
     RouterModule.forChild(routes),
     CommonModule
   ],
