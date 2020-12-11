63b213d8147072d8ae904c6ac940e2ced26822ad
Mariano <marianoap22@gmail.com>
2020-12-11 02:11:21 -0300

    autenticacion de login

--- src/app/components/routes/template/pages/login/login.component.ts
+++ src/app/components/routes/template/pages/login/login.component.ts
@@ -5,6 +5,9 @@ import { CustomValidators } from 'ngx-custom-validators';
 import { UserI } from 'src/app/interfaces/user-i';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { LoginService } from 'src/app/services/login/login.service';
+import { TokenService } from 'src/app/services/token/token.service';
+import { Router } from '@angular/router';
+import { LoginUsuario } from 'src/app/models/login-usuario/login-usuario';
 
 @Component({
     selector: 'app-login',
@@ -13,9 +16,18 @@ import { LoginService } from 'src/app/services/login/login.service';
 })
 export class LoginComponent implements OnInit {
 
+    isLogged=false;
+    isLoginFail=false;
+    loginUsuario: LoginUsuario;
+    nombreUsuario:string;
+    password:string;
+    errorMessage:string;
+
+    roles:string[]=[];
+
     valForm: FormGroup;
 
-    constructor(public settings: SettingsService, fb: FormBuilder, private authService:AuthService, private loginService: LoginService) {
+    constructor(public settings: SettingsService, fb: FormBuilder, private authService:AuthService, private tokenService:TokenService,private router:Router) {
 
         this.valForm = fb.group({
             'email': [null, Validators.compose([Validators.required, CustomValidators.email])],
@@ -24,7 +36,7 @@ export class LoginComponent implements OnInit {
 
     }
 
-    submitForm($ev, value: UserI) {
+    submitForm($ev, value) {
         $ev.preventDefault();
         for (let c in this.valForm.controls) {
             this.valForm.controls[c].markAsTouched();
@@ -32,19 +44,50 @@ export class LoginComponent implements OnInit {
         if (this.valForm.valid) {
             console.log('Valid!');
             console.log(value);
-            this.loginService.login();
+            this.nombreUsuario=value.email;
+            this.password=value.password;
+            this.onLogin();
+            //this.loginService.login();
             //this.authService.loginByEmail(value);
         }
     }
 
     ngOnInit() {
+        if(this.tokenService.getToken()){
+            this.isLogged=true;
+            this.isLoginFail=false;
+            this.roles=this.tokenService.getAuthorities();
+        }
+    }
 
+    onLogin(){
+        this.loginUsuario=new LoginUsuario(this.nombreUsuario,this.password);
+        this.authService.login(this.loginUsuario).subscribe(
+            data=> {
+                this.isLogged=true;
+                this.isLoginFail=false;
+
+                this.tokenService.setToken(data.token);
+                this.tokenService.setUserName(data.nombreUsuario);
+                this.tokenService.setAuthorities(data.authorities);
+                this.roles=data.authorities;
+                //preguntar si es familia
+                this.router.navigate(['/mis-arboles']);
+            },
+            err =>{
+                this.isLogged=false;
+                this.isLoginFail=true;
+                this.errorMessage=err.error.message;
+                console.log(this.errorMessage);
+            }
+
+        )
     }
     // public login(){
     //     this.loginService.login();
     // }
-    public logout(){
-        this.loginService.logout();
-    }
+    // public logout(){
+    //     this.loginService.logout();
+    // }
 
 }
---
+++ src/app/models/nuevo-usuario/nuevo-usuario.ts
@@ -0,0 +1,7 @@
+export class NuevoUsuario {
+    nombre:string;
+    nombreusuario:string;
+    email:string;
+    password:string;
+    authorities: string[];
+}
